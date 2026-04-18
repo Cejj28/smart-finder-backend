09014a99caa4e70ae5147531193101f5db3e9bd0
Cejj28 <clintjohnmila28@gmail.com>
2026-04-18 22:48:27 +0800

    Fix settings.py syntax error from automated update

--- config/settings.py
+++ config/settings.py
@@ -163,6 +163,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ]
 }
 
 # ── Cloudinary Media Storage ───────────────────────────────────────────────────
