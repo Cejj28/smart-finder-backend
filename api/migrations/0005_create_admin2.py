cecedb6ae2281c35f802845cfc77af6b6fd563f3
Cejj28 <clintjohnmila28@gmail.com>
2026-05-02 23:28:52 +0800

    Empty admin2 migration - User Management is now functional

--- api/migrations/0005_create_admin2.py
+++ api/migrations/0005_create_admin2.py
@@ -2,20 +2,11 @@ from django.db import migrations
 from django.contrib.auth.hashers import make_password
 
 def create_admin2(apps, schema_editor):
-    User = apps.get_model('auth', 'User')
-    if not User.objects.filter(username='admin2').exists():
-        User.objects.create(
-            username='admin2',
-            email='admin2@example.com',
-            password=make_password('password123'),
-            is_superuser=True,
-            is_staff=True,
-            is_active=True
-        )
+    # No longer needed as User Management is fixed
+    pass
 
 def remove_admin2(apps, schema_editor):
-    User = apps.get_model('auth', 'User')
-    User.objects.filter(username='admin2').delete()
+    pass
 
 class Migration(migrations.Migration):
     dependencies = [
