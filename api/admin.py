6b29eaf5b2c8128defbdff067caf0e7c47b0fef8
Cejj28 <clintjohnmila28@gmail.com>
2026-05-10 00:48:30 +0800

    Register Notifications in Admin

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Item, UserProfile
+from .models import Item, UserProfile, Notification
 
 # Inline so UserProfile appears directly inside the User detail page
 class UserProfileInline(admin.StackedInline):
@@ -29,3 +29,9 @@ class ItemAdmin(admin.ModelAdmin):
     list_display = ('type', 'item_name', 'location', 'reporter', 'created_at')
     list_filter = ('type', 'created_at')
     search_fields = ('item_name', 'location', 'description')
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'is_read', 'created_at')
+    list_filter = ('is_read', 'created_at')
+    search_fields = ('user__username', 'title', 'message')
